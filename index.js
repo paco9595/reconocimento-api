991c3c9396929512a818ef7401b3c339fda5870c
Francisco Villarreal <paco_vip95@hotmail.com>
2018-11-03 17:39:37 -0600

    add api

---
+++ index.js
@@ -0,0 +1,34 @@
+const exec = require('child_process').exec;
+const express = require('express');
+const app = express();
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(fileUpload());
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    return res.status(200).sendFile(__dirname + '/index.html')
+})
+
+app.post('/', function (req, res) {
+    const url = req.body.url;
+    if (!url) return res.status(400).send({ msg: 'No.' });
+    const img = url.split(';base64,').pop();
+    fs.writeFile('./img/img.png', img, { encoding: 'base64' }, function (err) {
+        if (err) return res.status(500).send({ msg: 'imagen no gusrdad' })
+        console.log('File created');
+        exec("python hola.py", function (err, stdout, stderr) {
+            if (err) return res.status(500).send({ msg: 'error al ejecutar' })
+            console.log(stdout);
+        });
+    });
+});
+
+app.listen(3000, () => {
+    console.log('done')
+});
+
